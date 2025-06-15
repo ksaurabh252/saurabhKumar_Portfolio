1f541c24fadb09025937fe1b663b01845e4b7945
ksaurabh252 <ksaurabh252@gmail.com>
2025-06-16 00:10:19 +0530

    url changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/saurabh-porfolio/",
+  base: "/saurabhKumar_Portfolio/",
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
